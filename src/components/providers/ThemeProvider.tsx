f1999e907f485ba093309b30ec4fed69773c2082
adyaakob <andylie2004@gmail.com>
2024-11-15 11:52:53 +0800

    Fix ThemeProvider type definitions

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,8 +1,25 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props} enableSystem={true} attribute="class">{children}</NextThemesProvider>
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+}
+
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: ThemeProviderProps) {
+  return (
+    <NextThemesProvider 
+      {...props} 
+      enableSystem={true} 
+      attribute="class"
+    >
+      {children}
+    </NextThemesProvider>
+  )
 } 
