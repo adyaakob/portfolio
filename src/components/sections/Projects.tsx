3ee5cadb9a7a66f540add4b45cec0bf53f75c7f3
adyaakob <andylie2004@gmail.com>
2024-11-15 12:02:16 +0800

    Update Project interface with date property

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,40 +1,56 @@
 import Image from 'next/image'
 import SectionTitle from '../SectionTitle'
 
+// Define the interface for project data
+interface Project {
+  title: string;
+  period: string;
+  date: string;
+  image: string;
+  description: string;
+  // Add any other properties your projects might have
+}
+
 const projectsData = [
   {
     title: "Combat Net Radio CNR9 and CNR10",
     period: "Jan 2010 - Jan 2020",
+    date: "Jan 2010 - Jan 2020",
     image: "/images/cnr9-10.jpg",
     description: ""
   },
   {
     title: "TRC5200 Tactical VHF Handheld",
     period: "Jan 2017 - Jan 2020",
+    date: "Jan 2017 - Jan 2020",
     image: "/images/trc5200.jpg",
     description: "Second generation of VHF radio with Sapura Thales Electronics (STE), fully designed and developed in Malaysia"
   },
   {
     title: "Future Soldier System (FSS)",
     period: "Jan 2015 - Jan 2018",
+    date: "Jan 2015 - Jan 2018",
     image: "/images/fss.jpg",
     description: "Fully developed by local engineers"
   },
   {
     title: "OICS for Deftech 8x8 GEMPITA",
     period: "Jan 2015 - Jan 2018",
+    date: "Jan 2015 - Jan 2018",
     image: "/images/oics.jpg",
     description: "Thales, through its joint venture STE, is part of the AV8 programme for the Royal Malaysian Army. STE provides communication, vetronics, sensors, command & control, mortar, and optronics"
   },
   {
     title: "Radio Communication Installation for 4x4 Lipan Bara",
     period: "Jan 2015 - Dec 2015",
+    date: "Jan 2015 - Dec 2015",
     image: "/images/lipan-bara.jpg",
     description: ""
   },
   {
     title: "TRC5100 Tactical VHF Handheld",
     period: "Jan 1997 - Jan 2015",
+    date: "Jan 1997 - Jan 2015",
     image: "/images/trc5100.jpg",
     description: "First-generation of tactical handheld, designed and produced locally"
   }
