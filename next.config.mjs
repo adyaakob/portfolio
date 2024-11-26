6319cd7a84a16f0aaaa63e77e48fe4c999b5218f
adyaakob <andylie2004@gmail.com>
2024-11-26 23:18:22 +0800

    Updated Next.js config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['flagcdn.com'],
+    unoptimized: true,
   },
+  basePath: '/portfolio',
 };
 
 export default nextConfig;
